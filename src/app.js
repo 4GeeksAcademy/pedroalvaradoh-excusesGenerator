5118fa42b132a997e482f2f9550b134d54e19386
Pedro Luis Alvarado Herrera <129191234+pedroalvaradoh@users.noreply.github.com>
2023-05-15 18:50:19 +0000

    feat: added excuse generator

--- src/app.js
+++ src/app.js
@@ -6,6 +6,30 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  // document.getElementById("excuse").innerHTML = ExqsGenerator();
+  document.querySelector("#excuseButton").addEventListener("click", function() {
+    document.getElementById("excuse").innerHTML = ExqsGenerator();
+  });
 };
+function ExqsGenerator() {
+  let who = ["The dog", "My grandma", "His turtle", "My bird", "Lucia"];
+  let action = ["ate", "peed", "crushed", "broke", "burned"];
+  let what = ["my homework", "the keys", "the car", "my house"];
+  let when = [
+    "before the class",
+    "right on time",
+    "when I finished",
+    "during my lunch",
+    "while I was praying"
+  ];
+
+  let whoRandom = who[Math.floor(Math.random() * who.length)];
+  let actionRandom = action[Math.floor(Math.random() * action.length)];
+  let whatRandom = what[Math.floor(Math.random() * what.length)];
+  let whenRandom = when[Math.floor(Math.random() * when.length)];
+
+  let randomExcuse =
+    whoRandom + " " + actionRandom + " " + whatRandom + " " + whenRandom;
+
+  return randomExcuse;
+}
